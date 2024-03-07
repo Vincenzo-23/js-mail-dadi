ca19997f463b283854e2d91d8830565ac9ede0c8
Vincenzo-23 <specialevincenzo88@gmail.com>
2024-03-07 14:57:02 +0100

    ADD: added pseudo code to the js inside the mail folder

--- mail/js/app.js
+++ mail/js/app.js
@@ -1,7 +1,28 @@
 console.log("inizio esercizio mail")
 
-//chiedere all'utente la propria mail
+//dichiaro una variabile e uso il querySelector per recuperare il button all'interno DOM tramite selettore (classe)
 
-//controllare che la mail sia presente nella lista di mail di chi può accedere
+//dichiaro una variabile e uso getElementById per recuperare l'input email all'interno del DOM tramite ID
 
-//stampare un messaggio appropriato in pagina con l'esito del controllo
+//creo un array con all'interno una serie di mail di persone che possono accedere
+
+//resto in ascolto per l'evento click sul mio button
+
+    //dichiaro una variabile in cui recupero il value dell'input mail che abbiamo chiesto all'utente (esempio: userMail)
+
+    //dichiaro una variabile di controllo per verificare la presenza della mail inserita dall'utente all'interno di quelle
+    //nel mio array
+
+    //creo un ciclo for per far scorrere ogni elemento dell'array (esempio: for (let i = 0; i < mails.length; i++){})
+
+    //salvo l'elemento attuale dell'array in una variabile (esempio: const mail = mails[i])
+
+
+    //controllo che la mail inserita dall'utente corrisponda all'elemento attuale dell'array
+    //quindi SE (userMail === mail)
+        //variabile di controllo = true
+
+    //SE l'utente ha inserito una mail presente tra quelle nell'array(variabile di controllo = true)
+        //stampo un messaggio appropriato in pagina con l'esito positivo
+    //ALTRIMENTI
+        //stampo un messaggio appropriato in pagina con l'esito negativo

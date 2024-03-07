cf4a31c7f9effed670b82e4f54c03e1da8e875da
Vincenzo-23 <specialevincenzo88@gmail.com>
2024-03-07 13:56:08 +0100

    ADD: added the 2 folders with the html and js files

---
+++ mail/js/app.js
@@ -0,0 +1,7 @@
+console.log("inizio esercizio mail")
+
+//chiedere all'utente la propria mail
+
+//controllare che la mail sia presente nella lista di mail di chi può accedere
+
+//stampare un messaggio appropriato in pagina con l'esito del controllo
